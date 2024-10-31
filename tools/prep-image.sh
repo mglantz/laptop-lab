d5b1a50dce4ff26dbdeb8ffe4549e2bdd273ebf9
Magnus Glantz <sudo@redhat.com>
2024-10-31 13:43:12 +0100

    Update prep-image.sh
    Hard coded path

--- tools/prep-image.sh
+++ tools/prep-image.sh
@@ -2,4 +2,4 @@
 # Prep KVM installation image, downloaded from:
 # https://access.redhat.com/downloads/content/479/ver=/rhel---9/9.1/x86_64/product-software
 
-virt-sysprep --root-password password:redhat123 --ssh-inject root:file:/home/mglantz/.ssh/id_rsa.pub --enable customize -a rhel91-template.qcow2
+virt-sysprep --root-password password:redhat123 --ssh-inject root:file:$HOME/.ssh/id_rsa.pub --enable customize -a rhel91-template.qcow2
